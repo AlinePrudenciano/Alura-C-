20fd1b5166c7ac5774e7785341b75ede13feceb6
AlinePrudenciano <82045238+AlinePrudenciano@users.noreply.github.com>
2022-02-22 18:38:14 -0300

    Conversões e outros tipos numericos

---
+++ ConversoesEOutrosTiposNumericos/ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1270.50;
+            int valor = (int) salario;
+
+            Console.WriteLine(valor);
+
+            double valor1 = 0.2;
+            double valor2 = 0.1;
+            double total = valor1 + valor2;
+
+            Console.WriteLine(total);
+
+            float pontoFlutuante = 3.14f;
+            double valore1 = 0.2;
+            double valore2 = 0.1;
+            double total1 = valore1 + valore2;
+            Console.WriteLine(total1);
+            Console.WriteLine(pontoFlutuante);
+
+            Console.Read();
+
+        
+        }
+    }
+}
