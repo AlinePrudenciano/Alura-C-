4eee553ecbd5f96bda7da109caa7a34139fa1baa
AlinePrudenciano <82045238+AlinePrudenciano@users.noreply.github.com>
2022-02-22 18:37:36 -0300

    Criando variáveis

---
+++ Criando Variáveis/Criando Variáveis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criando_Variáveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, novo teste.");
+
+            int idade = 31;
+
+            Console.WriteLine("Idade é " + idade);
+
+        }
+    }
+}
