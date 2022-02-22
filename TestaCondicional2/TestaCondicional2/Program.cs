7644345ab5b9df837ad0d56b72fef7d1dcd1d82a
AlinePrudenciano <82045238+AlinePrudenciano@users.noreply.github.com>
2022-02-22 18:39:42 -0300

    Outros testes de condicional

---
+++ TestaCondicional2/TestaCondicional2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestaCondicional2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testando condicionais");
+
+            int idade = 16;
+            int quantidadePessoas = 3;
+
+            if (idade >= 18 || quantidadePessoas >= 2)
+
+            {
+                Console.WriteLine("Você tem mais de 18 anos");
+                Console.WriteLine("Seja bem-vindo!");
+            }
+            else
+            {
+                Console.WriteLine("Infelizmente você não pode entrar.");
+            }
+            Console.Read();
+        }
+    }
+}
