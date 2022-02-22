8e1c05d3f4debb1e1b6ec85b7bd97b354445cd72
AlinePrudenciano <82045238+AlinePrudenciano@users.noreply.github.com>
2022-02-22 18:36:36 -0300

    Exercício de ponto flutuante

---
+++ TestaPontoFlutuante1/TestaPontoFlutuante1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestaPontoFlutuante1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 2500.37;
+            Console.WriteLine("Meu salário é de " + salario);
+
+            int divisao = (int) salario / 2;
+            Console.WriteLine("O resultado da divisão é " + divisao);
+            Console.ReadLine();
+
+        }
+    }
+}
